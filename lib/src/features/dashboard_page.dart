808ec6c85ee8c4b34ab9fdbef388af0aedd6caec
SherazAli-67 <soomrosheraz054@gmail.com>
2025-09-18 23:00:07 +0500

    Hitch Admin Panel
    1. Dashboard Development

--- lib/src/features/dashboard_page.dart
+++ lib/src/features/dashboard_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hitch_tracker/src/res/app_colors.dart';
@@ -79,8 +80,10 @@ class DashboardPage extends StatelessWidget{
                               spacing: 20,
                               children: [
                                 CircleAvatar(
+                                  radius: 30,
                                   // backgroundImage: NetworkImage(''),
                                   backgroundColor: AppColors.textFieldFillColor,
+                                  backgroundImage: CachedNetworkImageProvider('https://images.unsplash.com/photo-1758061607997-9acb866c12e3?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D'),
                                 ),
                                 Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
