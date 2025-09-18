566f7edb654c0e6604b3eddfd81a878daec47c89
SherazAli-67 <soomrosheraz054@gmail.com>
2025-09-18 23:31:00 +0500

    Hitch Admin Panel
    1. User models added

--- lib/src/features/dashboard_page.dart
+++ lib/src/features/dashboard_page.dart
@@ -35,143 +35,99 @@ class DashboardPage extends StatelessWidget{
             spacing: 10,
             children: [
               Text("Users", style: AppTextStyles.headingTextStyle,),
-              TextField(
-               decoration: InputDecoration(
-                 enabledBorder: OutlineInputBorder(
-                   borderRadius: BorderRadius.circular(10),
-                   borderSide: BorderSide(color: Colors.transparent)
-                 ),
-                 focusedBorder: OutlineInputBorder(
-                     borderRadius: BorderRadius.circular(10),
-                     borderSide: BorderSide(color: Colors.transparent)
-                 ),
-                 fillColor: AppColors.textFieldFillColor,
-                 filled: true,
-                 hintText: "Search users",
-                 hintStyle: AppTextStyles.smallTextStyle.copyWith(color: Colors.grey),
-                 prefixIcon: Icon(Icons.search_sharp, color: Colors.grey,)
-               ),
-              ),
-              Expanded(child: Card(
-                elevation: 1,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5)
-                ),
-                color: Colors.white,
-                child: ListView.builder(
-                    itemCount: 10,
-                    itemBuilder: (ctx, index){
-                      return ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.white,
-                            elevation: 0,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.zero
-                            ),
-                            // shadowColor: Colors.transparent
-                            surfaceTintColor: Colors.white,
-                            overlayColor: Colors.amber
-                          ),
-                          onPressed: (){}, child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: Row(
-                              spacing: 20,
-                              children: [
-                                CircleAvatar(
-                                  radius: 30,
-                                  // backgroundImage: NetworkImage(''),
-                                  backgroundColor: AppColors.textFieldFillColor,
-                                  backgroundImage: CachedNetworkImageProvider('https://images.unsplash.com/photo-1758061607997-9acb866c12e3?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D'),
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  spacing: 5,
-                                  children: [
-                                    Text("Sheraz Ali",style: AppTextStyles.regularTextStyle.copyWith(fontWeight: FontWeight.w600)),
-                                    Text("San Francisco, CA", style: AppTextStyles.smallTextStyle,),
-                                    Text("Passionate About Tennis and pickleball playing"),
-                                    SizedBox(height: 10,),
-                                    Row(
-                                      spacing: 10,
-                                      children: [
-                                        _buildPlayerTypeItem(playerType: 'Pickleball'),
-                                        _buildPlayerTypeItem(playerType: 'Tennis')
-                                      ],
-                                    )
-                                  ],
-                                ),
+              _buildSearchTextField(),
+              _buildUsersList()
 
-                              ],
-                            ),
-                          ),
-                          Container(
-                            height: 1,
-                            color: Colors.black12,
-                          )
-                        ],
-                      ));
-                }),
-              ))
-              /*Expanded(
-                child: DataTable(
-                  showCheckboxColumn: false,
-                  horizontalMargin: 20,
-                  columnSpacing: 40,
-                  headingTextStyle: const TextStyle(fontWeight: FontWeight.w700, color: Colors.black,),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: Colors.grey.withOpacity(0.1)
-                    // border: Border.all(color: Colors.purple),
-                  ),
-                  columns: [
-                    DataColumn(label: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Text("Profile")
-                    )),
-                    DataColumn(label: Text("Name")),
-                    DataColumn(label: Text('Bio')),
-                    DataColumn(label: Text("Location")),
-
-                    DataColumn(label: Text("Player Type Pickleball")),
-                    DataColumn(label: Text("Player Type Tennis")),
-                    // DataColumn(label: Text(AppLocalizations.of(context)!.yearsOfExperience,)),
-
-                  ], rows: List.generate(19, (index){
-                    return DataRow(
-                        onSelectChanged: (_){},
-                        cells: [
-                          DataCell(Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 4),
-                              child: CircleAvatar(
-                                backgroundColor: Colors.amber,
-                              )
-                          )),
-                          DataCell(Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 4),
-                              child: SizedBox(
-                                // width: 100,
-                                  child: TableItemWidget(text: "Sheraz Ali", isCenterAlignment: false,))
-                          )),
-                          DataCell(TableItemWidget(text: "Passionate Pickleball player and tennis", isCenterAlignment: false,)),
-                          DataCell(SizedBox(
-                              width: 100,
-                              child: TableItemWidget(text: 'San Fransisco, CA'))),
-                          DataCell(Center(child: TableItemWidget(text:'Pickle ball player'))),
-                          DataCell(Center(child: TableItemWidget(text:'Tennis ball player'))),
-                          // DataCell(TableItemWidget(text: getFormattedDate(user.createdAt.toDate()),textAlign: TextAlign.left,)),
-
-
-                        ]);
-                }),
-                ),
-              ),*/
             ],
           ),
         )
       ],
     );
+  }
+
+  Expanded _buildUsersList() {
+    return Expanded(child: Card(
+              elevation: 1,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5)
+              ),
+              color: Colors.white,
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (ctx, index){
+                    return ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.zero
+                          ),
+                          // shadowColor: Colors.transparent
+                          surfaceTintColor: Colors.white,
+                          overlayColor: Colors.grey[300]
+                        ),
+                        onPressed: (){}, child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Row(
+                            spacing: 20,
+                            children: [
+                              CircleAvatar(
+                                radius: 30,
+                                // backgroundImage: NetworkImage(''),
+                                backgroundColor: AppColors.textFieldFillColor,
+                                backgroundImage: CachedNetworkImageProvider('https://images.unsplash.com/photo-1758061607997-9acb866c12e3?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D'),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                spacing: 5,
+                                children: [
+                                  Text("Sheraz Ali",style: AppTextStyles.regularTextStyle.copyWith(fontWeight: FontWeight.w600)),
+                                  Text("San Francisco, CA", style: AppTextStyles.smallTextStyle,),
+                                  Text("Passionate About Tennis and pickleball playing"),
+                                  SizedBox(height: 10,),
+                                  Row(
+                                    spacing: 10,
+                                    children: [
+                                      _buildPlayerTypeItem(playerType: 'Pickleball'),
+                                      _buildPlayerTypeItem(playerType: 'Tennis')
+                                    ],
+                                  )
+                                ],
+                              ),
+
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: 1,
+                          color: Colors.black12,
+                        )
+                      ],
+                    ));
+              }),
+            ));
+  }
+
+  TextField _buildSearchTextField() {
+    return TextField(
+             decoration: InputDecoration(
+               enabledBorder: OutlineInputBorder(
+                 borderRadius: BorderRadius.circular(10),
+                 borderSide: BorderSide(color: Colors.transparent)
+               ),
+               focusedBorder: OutlineInputBorder(
+                   borderRadius: BorderRadius.circular(10),
+                   borderSide: BorderSide(color: Colors.transparent)
+               ),
+               fillColor: AppColors.textFieldFillColor,
+               filled: true,
+               hintText: "Search users",
+               hintStyle: AppTextStyles.smallTextStyle.copyWith(color: Colors.grey),
+               prefixIcon: Icon(Icons.search_sharp, color: Colors.grey,)
+             ),
+            );
   }
 
   Widget _buildPlayerTypeItem({required String playerType}) {
---
+++ lib/src/models/user_model.dart
@@ -0,0 +1,233 @@
+import 'package:hitch/src/models/coach_experience_model.dart';
+import 'package:hitch/src/models/uploaded_file_model.dart';
+
+import '../res/string_constants.dart';
+import 'player_level_model.dart';
+import 'package:timeago/timeago.dart' as timeago;
+
+class UserModel {
+  String userID;
+  String userName;
+  String profilePicture;
+  bool playerTypePickle;
+  bool playerTypeTennis;
+  bool playerTypePadel;
+  bool playerTypeCoach;
+  String level;
+  String bio;
+  String cellNumber;
+  String emailAddress;
+  String? age;
+  String? experience;
+  double distanceFromCurrentLocation;
+  final String token;
+  List<String> requestSentToUserIDs;
+  List<String> hiddenIds;
+  List<String> requestReceivedFromUserIDs;
+  String? gender;
+  final bool isReviewed;
+  List<String> declinedRequestsUserIDs;
+  double? latitude;
+  double? longitude;
+  PlayerLevelModel? pickleBallPlayerLevel;
+  PlayerLevelModel? tennisBallPlayerLevel;
+  PlayerLevelModel? padelBallPlayerLevel;
+  CoachExperienceModel? coachPickleBallExperienceLevel;
+  CoachExperienceModel? coachTennisBallExperienceLevel;
+  CoachExperienceModel? coachPadelBallExperienceLevel;
+  List<UploadedFileModel> uploadedSportsPhotos;
+  bool isAvailableDaily;
+  bool isAvailableInMorning;
+  String matchType;
+  String genderType;
+  String? myDuprID;
+  bool isConnectedToDupr;
+  double? duprSingleRating;
+  double? duprDoubleRating;
+  List<String> availableDaysToPlay;
+  bool showEventsNoti = true;
+  int? lastActive;
+  String get lastActiveTime {
+    if (lastActive == null) {
+      return '';
+    }
+    DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(lastActive!);
+    return timeago.format(dateTime, locale: 'en').replaceAll('about ', '');
+  }
+
+  UserModel(
+      {required this.userID,
+      required this.userName,
+      required this.profilePicture,
+      required this.playerTypePickle,
+      required this.playerTypeTennis,
+      required this.playerTypePadel,
+      required this.playerTypeCoach,
+      this.level = '',
+      required this.bio,
+      required this.cellNumber,
+      required this.emailAddress,
+      this.latitude,
+      this.longitude,
+      this.gender,
+      this.distanceFromCurrentLocation = 10,
+      this.age,
+      this.token = '',
+      this.experience,
+      this.requestReceivedFromUserIDs = const [],
+      this.hiddenIds = const [],
+      this.declinedRequestsUserIDs = const [],
+      this.requestSentToUserIDs = const [],
+      this.coachPickleBallExperienceLevel,
+      this.coachTennisBallExperienceLevel,
+      this.coachPadelBallExperienceLevel,
+      this.pickleBallPlayerLevel,
+      this.tennisBallPlayerLevel,
+      this.padelBallPlayerLevel,
+      this.uploadedSportsPhotos = const [],
+      this.isAvailableDaily = true,
+      this.isAvailableInMorning = false,
+      this.matchType = 'Both',
+      this.genderType = 'Both',
+      this.myDuprID,
+      this.isConnectedToDupr = false,
+      this.duprDoubleRating,
+      this.duprSingleRating,
+      required this.availableDaysToPlay,
+      this.showEventsNoti = true,
+      this.isReviewed = false,
+      this.lastActive});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'userID': userID,
+      'userName': userName,
+      'profilePicture': profilePicture,
+      'playerTypePickle': playerTypePickle,
+      'playerTypeTennis': playerTypeTennis,
+      'playerTypePadel': playerTypePadel,
+      'playerTypeCoach': playerTypeCoach,
+      'level': level,
+      'bio': bio,
+      'cellNumber': cellNumber,
+      'emailAddress': emailAddress,
+      'age': age,
+      'experience': experience,
+      'token': token,
+      'distanceFromCurrentLocation': distanceFromCurrentLocation,
+      'requestSentToUserIDs': requestSentToUserIDs,
+      'hiddenIds': hiddenIds,
+      'requestReceivedFromUserIDs': requestReceivedFromUserIDs,
+      'declinedRequestsUserIDs': requestReceivedFromUserIDs,
+      // 'languages' : languages,
+      'latitude': latitude,
+      'longitude': longitude,
+      'gender': gender,
+      'myDuprID': myDuprID,
+      'isConnectedToDupr': isConnectedToDupr,
+      'duprDoubleRating': duprDoubleRating,
+      'duprSingleRating': duprSingleRating,
+      'coachPickleBallExperienceLevel': coachPickleBallExperienceLevel?.toMap(),
+      'coachTennisBallExperienceLevel': coachTennisBallExperienceLevel?.toMap(),
+      'coachPadelBallExperienceLevel': coachPadelBallExperienceLevel?.toMap(),
+      'pickleBallPlayerLevel': pickleBallPlayerLevel?.toMap(),
+      'tennisBallPlayerLevel': tennisBallPlayerLevel?.toMap(),
+      'padelBallPlayerLevel': padelBallPlayerLevel?.toMap(),
+      'showEventsNoti': showEventsNoti,
+      uploadedFilesKey: uploadedSportsPhotos
+          .map((uploadedSportsPhotos) => uploadedSportsPhotos.toMap())
+          .toList(),
+      isAvailableDailyKey: isAvailableDaily,
+      isAvailableInMorningKey: isAvailableInMorning,
+      matchTypeKey: matchType,
+      genderTypeKey: genderType,
+      availableDaysToPlayKey: availableDaysToPlay,
+      isReviewedKey: isReviewed,
+      'lastActive': lastActive,
+    };
+  }
+
+  // Create a UserProfile object from a map
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+        userID: map['userID'],
+        userName: map['userName'],
+        profilePicture: map['profilePicture'],
+        playerTypePickle: map['playerTypePickle'] ?? true,
+        playerTypeTennis: map['playerTypeTennis'] ?? true,
+        playerTypePadel: map['playerTypePadel'] ?? true,
+        playerTypeCoach: map['playerTypeCoach'] ?? false,
+        level: map['level'],
+        bio: map['bio'],
+        cellNumber: map['cellNumber'],
+        emailAddress: map['emailAddress'],
+        age: map['age'],
+        experience: map['experience'],
+        token: map['token'] ?? '',
+        distanceFromCurrentLocation: map['distanceFromCurrentLocation'] > 20
+            ? 20
+            : map['distanceFromCurrentLocation'],
+        requestSentToUserIDs:
+            List<String>.from(map['requestSentToUserIDs'] ?? []),
+        hiddenIds:
+        List<String>.from(map['hiddenIds'] ?? []),
+        declinedRequestsUserIDs:
+            List<String>.from(map['declinedRequestsUserIDs'] ?? []),
+        // languages: List<String>.from(map['languages'] ?? []),
+        requestReceivedFromUserIDs:
+            List<String>.from(map['requestReceivedFromUserIDs'] ?? []),
+        latitude: map['latitude'],
+        longitude: map['longitude'],
+        gender: map['gender'],
+        myDuprID: map['myDuprID'],
+        isConnectedToDupr: map['isConnectedToDupr'] ?? false,
+        duprDoubleRating: map['duprDoubleRating'],
+        duprSingleRating: map['duprSingleRating'],
+        coachPickleBallExperienceLevel:
+            map['coachPickleBallExperienceLevel'] != null
+                ? CoachExperienceModel.fromMap(
+                    map['coachPickleBallExperienceLevel'])
+                : null,
+        coachTennisBallExperienceLevel:
+            map['coachTennisBallExperienceLevel'] != null
+                ? CoachExperienceModel.fromMap(
+                    map['coachTennisBallExperienceLevel'])
+                : null,
+        coachPadelBallExperienceLevel:
+        map['coachPadelBallExperienceLevel'] != null
+            ? CoachExperienceModel.fromMap(
+            map['coachPadelBallExperienceLevel'])
+            : null,
+        pickleBallPlayerLevel: map['pickleBallPlayerLevel'] != null
+            ? PlayerLevelModel.fromMap(map['pickleBallPlayerLevel'])
+            : null,
+        tennisBallPlayerLevel: map['tennisBallPlayerLevel'] != null
+            ? PlayerLevelModel.fromMap(map['tennisBallPlayerLevel'])
+            : null,
+        padelBallPlayerLevel: map['padelBallPlayerLevel'] != null
+            ? PlayerLevelModel.fromMap(map['padelBallPlayerLevel'])
+            : null,
+        uploadedSportsPhotos: (map['uploadedFiles'] as List<dynamic>? ?? [])
+            .map((fileMap) =>
+                UploadedFileModel.fromMap(fileMap as Map<String, dynamic>))
+            .toList(),
+        isAvailableDaily: map[isAvailableDailyKey] ?? true,
+        showEventsNoti: map['showEventsNoti'] ?? true,
+        isAvailableInMorning: map[isAvailableInMorningKey] ?? false,
+        matchType: map[matchTypeKey] ?? 'Both',
+        genderType: map[genderTypeKey] ?? 'Both',
+        isReviewed: map[isReviewedKey] ?? false,
+        lastActive: map['lastActive'],
+        availableDaysToPlay: List<String>.from(
+          map[availableDaysToPlayKey] ?? [],
+        ));
+  }
+
+  void setUserID({required String userID}) {
+    this.userID = userID;
+  }
+
+  void setProfileUrl({required String profilePicture}) {
+    this.profilePicture = profilePicture;
+  }
+}
