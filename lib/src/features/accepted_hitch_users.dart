1aacdea4ee2db582b9f63422cdd917678839c9a3
SherazAli-67 <soomrosheraz054@gmail.com>
2025-09-20 14:04:19 +0500

    Hitch Admin Panel : Accepted Hitch Users
    1. Accepted Hitches Development

--- lib/src/features/accepted_hitch_users.dart
+++ lib/src/features/accepted_hitch_users.dart
@@ -1,10 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:hitch_tracker/src/models/hitch_tracker_model.dart';
+import 'package:hitch_tracker/src/models/accepted_hitch_user_model.dart';
 import 'package:hitch_tracker/src/res/app_colors.dart';
 import 'package:hitch_tracker/src/res/app_textstyles.dart';
-import 'dart:async';
 
 
 class AcceptedHitchRequestsPage extends StatefulWidget {
